9f873fe828953b72854f73cff7dd3f4ea55f58bf
perkles <deboramacedo8816@gmail.com>
2022-02-24 22:38:22 -0300

    1004

---
+++ 1004produto_simples.js
@@ -0,0 +1,11 @@
+//Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.   
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+    
+var a = parseInt(lines.shift());
+var b = parseInt(lines.shift());
+   
+console.log('PROD = ' + (a*b));
+
+//produto simples
