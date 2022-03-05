de924c3940f9d95d1767c414d57d1be67a2ebf49
perkles <deboramacedo8816@gmail.com>
2022-03-05 19:29:36 -0300

    1020

--- 1020idade-em-dias.js
+++ 1020idade-em-dias.js
@@ -1,3 +1,16 @@
 //Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias
 
-var n = 400
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var n = parseInt(lines.shift())
+var ano = parseInt(n/365)
+n= n-(ano*365)
+var mes= parseInt(n/30)
+n= n-(mes*30)
+var dias = n
+console.log(ano + ' ano(s)')
+console.log(mes + ' mes(es)')
+console.log(dias + ' dia(s)')
+
+//idade em dias
